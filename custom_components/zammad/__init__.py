3733b9dd8db8f4caeba645a467ac6f2964ead4e8
Thomas J Leach <tj@metrics.consulting>
2024-08-07 18:58:29 -0400

    Fix deprecation warning

--- custom_components/zammad/__init__.py
+++ custom_components/zammad/__init__.py
@@ -1,6 +1,7 @@
 """
 Custom integration to integrate Zammad with Home Assistant.
 """
+
 from aiohttp.client_exceptions import ClientConnectorError
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -50,10 +51,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        hass.async_add_job(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.add_update_listener(async_reload_entry)
     return True
